54fff3354684df6721baebbc70d0b918a05886b4
Sekoree <me@sekoree.dev>
2024-12-18 23:46:28 +0100

    Initial commit

---
+++ PlayerButtons/Program.cs
@@ -0,0 +1,34 @@
+using PlayerButtons.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//Bind to any IP address
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+#if DEBUG
+app.Run();
+#else
+app.Run("http://192.168.137.1:5000");
+#endif
